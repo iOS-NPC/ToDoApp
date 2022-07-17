d19d4b7e180967c8b5967c5d0df1e74119af877f
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2022-07-17 21:08:36 +0900

    main view

---
+++ ToDoApp/ToDoCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoCell.swift
+//  ToDoApp
+//
+//  Created by 남경민 on 2022/07/17.
+//
+
+import UIKit
+
+class ToDoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var toDoTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.title = "DO!"
+        self.makeNavigationBar()
+        
+        toDoTableView.delegate = self
+        toDoTableView.dataSource = self
+        
     }
-
-
+    
+    func makeNavigationBar() {
+        let item = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewTodo))
+        
+        navigationItem.rightBarButtonItem = item
+    }
+    
+    @objc func addNewTodo() {
+        
+    }
 }
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
+        return cell
+    }
+}
+
 
